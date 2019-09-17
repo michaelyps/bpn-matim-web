afb4602b85d47223c669bacaa3409fadc1a6d77b
Michael Yosavani Putra <surbaktievan@ymail.com>
2019-09-17 22:04:38 +0700

    adding gallery

--- src/Pages/galeri.js
+++ src/Pages/galeri.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 
-import { Segment, Container, Grid, Header, List } from "semantic-ui-react";
+import {
+  Segment,
+  Container,
+  Grid,
+  Header,
+  List,
+  Image
+} from "semantic-ui-react";
+
+import gallery1 from "../img/gallery1.jpg";
+import gallery2 from "../img/gallery2.jpg";
+import gallery3 from "../img/gallery3.jpg";
 
 import "../App.css";
 
@@ -16,30 +27,32 @@ class Galeri extends Component {
           <div>
             <h1 className="gallery-title">Galeri</h1>
           </div>
-          <Grid celled="internally" columns={3}>
-            <Grid.Row>
-              <Grid.Column>
-                <p />
-              </Grid.Column>
-              <Grid.Column>
-                <p />
-              </Grid.Column>
-              <Grid.Column>
-                <p />
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column>
-                <p />
-              </Grid.Column>
-              <Grid.Column>
-                <p />
-              </Grid.Column>
-              <Grid.Column>
-                <p />
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
+          <div className="isi-galeri">
+            <Grid columns={3}>
+              <Grid.Row>
+                <Grid.Column>
+                  <Image wrapped size="medium" src={gallery1} />
+                </Grid.Column>
+                <Grid.Column>
+                  <Image wrapped size="medium" src={gallery2} />
+                </Grid.Column>
+                <Grid.Column>
+                  <Image wrapped size="medium" src={gallery3} />
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column>
+                  <p />
+                </Grid.Column>
+                <Grid.Column>
+                  <p />
+                </Grid.Column>
+                <Grid.Column>
+                  <p />
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </div>
         </Container>
       </div>
     );
--- src/Pages/layanan.js
+++ src/Pages/layanan.js
@@ -425,6 +425,365 @@ class Layanan extends Component {
                     </List>
                   </p>
                 </Accordion.Content>
+                <Accordion.Title
+                  active={activeIndex === 5}
+                  index={5}
+                  onClick={this.handleClick}
+                >
+                  <Icon name="dropdown" />
+                  Peralihan Hak Pembagian Hak Bersama
+                </Accordion.Title>
+                <Accordion.Content active={activeIndex === 5}>
+                  <p>
+                    <List as="ul">
+                      <List.Item>
+                        <h5>Persyaratan</h5>
+                      </List.Item>
+                      <List.Item as="li">
+                        Formulir Permohonan yang sudah diisi dan ditandatangani
+                        pemohon atau kuasanya diatas materai cukup
+                      </List.Item>
+                      <List.Item as="li">
+                        Surat kuasa apabila dikuasakan
+                      </List.Item>
+                      <List.Item as="li">
+                        Fotocopy identitas pemohon/para ahli waris (KTP,KK) dan
+                        kuasa apabila dikuasakan, yang telah dicocokkan dengan
+                        aslinya oleh petugas loket
+                      </List.Item>
+                      <List.Item as="li">
+                        Fotocopy akta pendirian dan pengesahan badan hukum yang
+                        telah dicocokkan dengan aslinya oleh petugas loket
+                      </List.Item>
+                      <List.Item as="li">Sertifikat Asli</List.Item>
+                      <List.Item as="li">
+                        Akta pembagian hak bersama dari PPAT
+                      </List.Item>
+                      <List.Item as="li">
+                        Ijin pemindahan hak apabila di dalam sertifikat /
+                        keputusannya dicantumkan tanda yang menyatakan bahwa hak
+                        tersebut hanya boleh dipindahtangankan jika telah
+                        diperoleh ijin dari instansi yang berwenang
+                      </List.Item>
+                      <List.Item as="li">
+                        Fotocopy SPPT dan PBB tahun berjalan yang telah
+                        dicocokkan dengan aslinya oleh petugas loket, penyerahan
+                        bukti SBB (BPHTB) dan bukti bayar uang pemasukan (pada
+                        saat pendaftaran hak)
+                      </List.Item>
+                      <List.Item as="li">
+                        Penyerahan bukti SSB (BPHTB) bukti SSP/PPH untuk
+                        perolehan tanah lebih dari 60 Juta Rupiah
+                      </List.Item>
+                      <br />
+                      <List.Item>
+                        <h5>Penyelesaian</h5>
+                      </List.Item>
+                      <List.Item as="li">5 hari kerja</List.Item>
+                      <br />
+                      <List.Item>
+                        <h5>Keterangan</h5>
+                      </List.Item>
+                      <List.Item as="li">Identitas diri</List.Item>
+                      <List.Item as="li">
+                        Luas, letak dan penggunaan tanah yang dimohon
+                      </List.Item>
+                      <List.Item as="li">
+                        Pernyataan tanah tidak sengketa
+                      </List.Item>
+                      <List.Item as="li">
+                        Pernyataan tanah/bangunan dikuasai secara fisik
+                      </List.Item>
+                      <br />
+                      <List.Item>
+                        <h5>Tarif</h5>
+                      </List.Item>
+                      <List.Item as="li">
+                        Biaya dihitung berdasarkan nilai tanah yang dikeluarkan
+                        oleh kantor pertanahan dengan rumus (nilai tanah(per m2)
+                        * luas tanah (m2)) / 1000
+                      </List.Item>
+                    </List>
+                  </p>
+                </Accordion.Content>
+                <Accordion.Title
+                  active={activeIndex === 6}
+                  index={6}
+                  onClick={this.handleClick}
+                >
+                  <Icon name="dropdown" />
+                  Peralihan Hak Pemasukan Dalam Perusahaan
+                </Accordion.Title>
+                <Accordion.Content active={activeIndex === 6}>
+                  <p>
+                    <List as="ul">
+                      <List.Item>
+                        <h5>Persyaratan</h5>
+                      </List.Item>
+                      <List.Item as="li">
+                        Formulir Permohonan yang sudah diisi dan ditandatangani
+                        pemohon atau kuasanya diatas materai cukup
+                      </List.Item>
+                      <List.Item as="li">
+                        Surat kuasa apabila dikuasakan
+                      </List.Item>
+                      <List.Item as="li">
+                        Fotocopy identitas pemohon/para ahli waris (KTP,KK) dan
+                        kuasa apabila dikuasakan, yang telah dicocokkan dengan
+                        aslinya oleh petugas loket
+                      </List.Item>
+                      <List.Item as="li">
+                        Fotocopy akta pendirian dan pengesahan badan hukum yang
+                        telah dicocokkan dengan aslinya oleh petugas loket, bagi
+                        badan hukum
+                      </List.Item>
+                      <List.Item as="li">Sertifikat Asli</List.Item>
+                      <List.Item as="li">
+                        Akta pembagian hak bersama dari PPAT
+                      </List.Item>
+                      <List.Item as="li">
+                        Ijin pemindahan hak apabila di dalam sertifikat /
+                        keputusannya dicantumkan tanda yang menyatakan bahwa hak
+                        tersebut hanya boleh dipindahtangankan jika telah
+                        diperoleh ijin dari instansi yang berwenang
+                      </List.Item>
+                      <List.Item as="li">
+                        Fotocopy SPPT dan PBB tahun berjalan yang telah
+                        dicocokkan dengan aslinya oleh petugas loket, penyerahan
+                        bukti SBB (BPHTB) dan bukti bayar uang pemasukan (pada
+                        saat pendaftaran hak)
+                      </List.Item>
+                      <List.Item as="li">
+                        Penyerahan bukti SSB (BPHTB) bukti SSP/PPH untuk
+                        perolehan tanah lebih dari 60 Juta Rupiah
+                      </List.Item>
+                      <br />
+                      <List.Item>
+                        <h5>Penyelesaian</h5>
+                      </List.Item>
+                      <List.Item as="li">5 hari kerja</List.Item>
+                      <br />
+                      <List.Item>
+                        <h5>Keterangan</h5>
+                      </List.Item>
+                      <List.Item as="li">Identitas diri</List.Item>
+                      <List.Item as="li">
+                        Luas, letak dan penggunaan tanah yang dimohon
+                      </List.Item>
+                      <List.Item as="li">
+                        Pernyataan tanah tidak sengketa
+                      </List.Item>
+                      <List.Item as="li">
+                        Pernyataan tanah/bangunan dikuasai secara fisik
+                      </List.Item>
+                      <br />
+                      <List.Item>
+                        <h5>Tarif</h5>
+                      </List.Item>
+                      <List.Item as="li">
+                        Biaya dihitung berdasarkan nilai tanah yang dikeluarkan
+                        oleh kantor pertanahan dengan rumus (nilai tanah(per m2)
+                        * luas tanah (m2)) / 1000
+                      </List.Item>
+                    </List>
+                  </p>
+                </Accordion.Content>
+                <Accordion.Title
+                  active={activeIndex === 7}
+                  index={7}
+                  onClick={this.handleClick}
+                >
+                  <Icon name="dropdown" />
+                  Pemecahan
+                </Accordion.Title>
+                <Accordion.Content active={activeIndex === 7}>
+                  <p>
+                    <List as="ul">
+                      <List.Item>
+                        <h5>Persyaratan</h5>
+                      </List.Item>
+                      <List.Item as="li">
+                        Formulir Permohonan yang sudah diisi dan ditandatangani
+                        pemohon atau kuasanya diatas materai cukup
+                      </List.Item>
+                      <List.Item as="li">
+                        Surat kuasa apabila dikuasakan
+                      </List.Item>
+                      <List.Item as="li">
+                        Fotocopy identitas pemohon/para ahli waris (KTP,KK) dan
+                        kuasa apabila dikuasakan, yang telah dicocokkan dengan
+                        aslinya oleh petugas loket
+                      </List.Item>
+                      <List.Item as="li">
+                        Fotocopy akta pendirian dan pengesahan badan hukum yang
+                        telah dicocokkan dengan aslinya oleh petugas loket, bagi
+                        badan hukum
+                      </List.Item>
+                      <List.Item as="li">Sertifikat Asli</List.Item>
+                      <List.Item as="li">
+                        Rencana Tapak/Site Plan dari Pemerintah Kabupaten/Kota
+                        setempat
+                      </List.Item>
+                      <br />
+                      <List.Item>
+                        <h5>Penyelesaian</h5>
+                      </List.Item>
+                      <List.Item as="li">15 hari kerja</List.Item>
+                      <br />
+                      <List.Item>
+                        <h5>Keterangan</h5>
+                      </List.Item>
+                      <List.Item as="li">Identitas diri</List.Item>
+                      <List.Item as="li">
+                        Luas, letak dan penggunaan tanah yang dimohon
+                      </List.Item>
+                      <List.Item as="li">
+                        Pernyataan tanah tidak sengketa
+                      </List.Item>
+                      <List.Item as="li">
+                        Pernyataan tanah/bangunan dikuasai secara fisik
+                      </List.Item>
+                      <List.Item as="li">Alasan Pemecahan</List.Item>
+                      <br />
+                      <List.Item>
+                        <h5>Tarif</h5>
+                      </List.Item>
+                      <List.Item as="li">
+                        Biaya dihitung berdasarkan jumlah bidang dan luas
+                        masing-masing bidang pemecahan
+                      </List.Item>
+                    </List>
+                  </p>
+                </Accordion.Content>
+                <Accordion.Title
+                  active={activeIndex === 8}
+                  index={8}
+                  onClick={this.handleClick}
+                >
+                  <Icon name="dropdown" />
+                  Pemisahan
+                </Accordion.Title>
+                <Accordion.Content active={activeIndex === 8}>
+                  <p>
+                    <List as="ul">
+                      <List.Item>
+                        <h5>Persyaratan</h5>
+                      </List.Item>
+                      <List.Item as="li">
+                        Formulir Permohonan yang sudah diisi dan ditandatangani
+                        pemohon atau kuasanya diatas materai cukup
+                      </List.Item>
+                      <List.Item as="li">
+                        Surat kuasa apabila dikuasakan
+                      </List.Item>
+                      <List.Item as="li">
+                        Fotocopy identitas pemohon/para ahli waris (KTP,KK) dan
+                        kuasa apabila dikuasakan, yang telah dicocokkan dengan
+                        aslinya oleh petugas loket
+                      </List.Item>
+                      <List.Item as="li">
+                        Fotocopy akta pendirian dan pengesahan badan hukum yang
+                        telah dicocokkan dengan aslinya oleh petugas loket, bagi
+                        badan hukum
+                      </List.Item>
+                      <List.Item as="li">Sertifikat Asli</List.Item>
+                      <List.Item as="li">
+                        Rencana Tapak/Site Plan dari Pemerintah Kabupaten/Kota
+                        setempat
+                      </List.Item>
+                      <br />
+                      <List.Item>
+                        <h5>Penyelesaian</h5>
+                      </List.Item>
+                      <List.Item as="li">15 hari kerja</List.Item>
+                      <br />
+                      <List.Item>
+                        <h5>Keterangan</h5>
+                      </List.Item>
+                      <List.Item as="li">Identitas diri</List.Item>
+                      <List.Item as="li">
+                        Luas, letak dan penggunaan tanah yang dimohon
+                      </List.Item>
+                      <List.Item as="li">
+                        Pernyataan tanah tidak sengketa
+                      </List.Item>
+                      <List.Item as="li">
+                        Pernyataan tanah/bangunan dikuasai secara fisik
+                      </List.Item>
+                      <List.Item as="li">Alasan Pemecahan</List.Item>
+                      <br />
+                      <List.Item>
+                        <h5>Tarif</h5>
+                      </List.Item>
+                      <List.Item as="li">
+                        Biaya dihitung berdasarkan jumlah bidang dan luas
+                        masing-masing bidang pemecahan
+                      </List.Item>
+                    </List>
+                  </p>
+                </Accordion.Content>
+                <Accordion.Title
+                  active={activeIndex === 9}
+                  index={9}
+                  onClick={this.handleClick}
+                >
+                  <Icon name="dropdown" />
+                  Penggabungan
+                </Accordion.Title>
+                <Accordion.Content active={activeIndex === 9}>
+                  <p>
+                    <List as="ul">
+                      <List.Item>
+                        <h5>Persyaratan</h5>
+                      </List.Item>
+                      <List.Item as="li">
+                        Formulir Permohonan yang sudah diisi dan ditandatangani
+                        pemohon atau kuasanya diatas materai cukup
+                      </List.Item>
+                      <List.Item as="li">
+                        Surat kuasa apabila dikuasakan
+                      </List.Item>
+                      <List.Item as="li">
+                        Fotocopy identitas pemohon (KTP) dan kuasa apabila
+                        dikuasakan, yang telah dicocokkan dengan aslinya oleh
+                        petugas loket
+                      </List.Item>
+                      <List.Item as="li">
+                        Fotocopy akta pendirian dan pengesahan badan hukum yang
+                        telah dicocokkan dengan aslinya oleh petugas loket, bagi
+                        badan hukum
+                      </List.Item>
+                      <List.Item as="li">Sertifikat Asli</List.Item>
+                      <br />
+                      <List.Item>
+                        <h5>Penyelesaian</h5>
+                      </List.Item>
+                      <List.Item as="li">15 hari kerja</List.Item>
+                      <br />
+                      <List.Item>
+                        <h5>Keterangan</h5>
+                      </List.Item>
+                      <List.Item as="li">Identitas diri</List.Item>
+                      <List.Item as="li">
+                        Luas, letak dan penggunaan tanah yang dimohon
+                      </List.Item>
+                      <List.Item as="li">
+                        Pernyataan tanah tidak sengketa
+                      </List.Item>
+                      <List.Item as="li">
+                        Pernyataan tanah/bangunan dikuasai secara fisik
+                      </List.Item>
+                      <List.Item as="li">Alasan Penggabungan</List.Item>
+                      <br />
+                      <List.Item>
+                        <h5>Tarif</h5>
+                      </List.Item>
+                      <List.Item as="li">
+                        Biaya dihitung berdasarkan luas penggabungan
+                      </List.Item>
+                    </List>
+                  </p>
+                </Accordion.Content>
               </Accordion>
             </div>
           </Container>
